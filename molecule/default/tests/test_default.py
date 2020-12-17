e9eeab1211ee8db87da16010eb3d83fea9ccf312
tstechly <tomasz@stechly.com>
2020-12-17 14:21:35 -0700

    fix references to role_name so files are generated from jinja. use wget as an example instead of vi because of some centos7 laments

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,8 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-# example test. FIXME
 @pytest.mark.parametrize('pkg', [
-  'vim'
+  'wget'
 ])
 def test_pkg(host, pkg):
     package = host.package(pkg)
